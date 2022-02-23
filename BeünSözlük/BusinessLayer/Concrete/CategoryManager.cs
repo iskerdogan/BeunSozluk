e56744f6af01096b95f9ddd4fa8e9b2eab40f10c
Erdoğan IŞIK <erdogani0202@gmail.com>
2022-02-24 02:44:06 +0300

    CategoryManager added to BusinessLayer

--- BeünSözlük/BusinessLayer/Concrete/CategoryManager.cs
+++ BeünSözlük/BusinessLayer/Concrete/CategoryManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,23 @@ namespace BusinessLayer.Concrete
 {
     public class CategoryManager
     {
+        GenericRepository<Category> repository = new GenericRepository<Category>();
+
+        public List<Category> GetAll()
+        {
+            return repository.List();
+        }
+
+        public void CategoryAdd(Category category)
+        {
+            if (category.CategoryName == "" || category.CategoryName.Length <= 3 || category.CategoryDescription == "" || category.CategoryName.Length >= 51)
+            {
+                //hata mesajı
+            }
+            else
+            {
+                repository.Add(category);   
+            }
+        }
     }
 }
