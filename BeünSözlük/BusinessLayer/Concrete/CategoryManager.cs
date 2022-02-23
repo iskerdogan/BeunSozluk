02a45cc28902a1e8102cb3663772eb159377185b
Erdoğan IŞIK <erdogani0202@gmail.com>
2022-02-24 02:38:56 +0300

    Revert "BusinessLayer added CategoryManager"
    This reverts commit d2c3008496ad29f3937bad84e6b2baa2787c99d2.

--- BeünSözlük/BusinessLayer/Concrete/CategoryManager.cs
+++ BeünSözlük/BusinessLayer/Concrete/CategoryManager.cs
@@ -1,6 +1,4 @@
-﻿using DataAccessLayer.Concrete.Repositories;
-using EntityLayer.Concrete;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,23 +8,5 @@ namespace BusinessLayer.Concrete
 {
     public class CategoryManager
     {
-        GenericRepository<Category> repository = new GenericRepository<Category>();
-
-        public List<Category> GetAll()
-        {
-            return repository.List();
-        }
-
-        public void CategoryAdd(Category category)
-        {
-            if (category.CategoryName=="" || category.CategoryName.Length<= 3 || category.CategoryDescription=="" || category.CategoryName.Length>=51)
-            {
-                //hata mesajı
-            }
-            else
-            {
-                repository.Add(category);
-            }
-        }
     }
 }
