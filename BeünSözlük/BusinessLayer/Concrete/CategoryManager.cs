8ed355c08d127e4e15b024ca84046ffe5ef45f24
Erdoğan IŞIK <erdogani0202@gmail.com>
2022-02-24 02:25:19 +0300

    Code refactoring

--- BeünSözlük/BusinessLayer/Concrete/CategoryManager.cs
+++ BeünSözlük/BusinessLayer/Concrete/CategoryManager.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BusinessLayer
+namespace BusinessLayer.Concrete
 {
-    public class Class1
+    public class CategoryManager
     {
     }
 }
