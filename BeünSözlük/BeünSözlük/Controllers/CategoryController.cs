6d4596aa4c6bef2e6d3da698e60bfe321659e135
Erdoğan IŞIK <erdogani0202@gmail.com>
2022-02-24 03:06:44 +0300

    Category listing added

---
+++ BeünSözlük/BeünSözlük/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeünSözlük.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        CategoryManager categoryManager = new CategoryManager();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetCategoyList()
+        {
+            var categoryValues = categoryManager.GetAll();
+            return View(categoryValues);
+        }
+    }
+}
