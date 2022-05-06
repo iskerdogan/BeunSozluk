7f8694d2cc81f186aa83636458f4a44848f0e617
Erdoğan IŞIK <erdogani0202@gmail.com>
2022-05-06 11:17:05 +0300

    About added.

---
+++ BeünSözlük/BeünSözlük/Controllers/AboutController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeünSözlük.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutDal());
+        public ActionResult Index()
+        {
+            var aboutValues= aboutManager.GetList();
+            return View(aboutValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddAbout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAbout(About about)
+        {
+            aboutManager.AddAboutBusinessLayer(about);
+            return RedirectToAction("Index");
+        }
+
+        public PartialViewResult AboutPartial()
+        {
+            return PartialView();
+        }
+    }
+}
